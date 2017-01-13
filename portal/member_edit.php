5fb8e2632253d7292bdcfd63f2fb9dfefcf36e09
sbruso <sbruso@itretail.com>
2017-01-13 16:39:08 -0500

    update member_edit in study groups to updated format

--- portal/member_edit.php
+++ portal/member_edit.php
@@ -1,10 +1,3 @@
-<?php
-include_once '../includes/db_connect.php';
-include_once '../includes/functions.php';
-
-sec_session_start();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -40,47 +33,28 @@ sec_session_start();
     </script>
     <title><?php echo date("Y"); ?> Boston Area Training on Eldership</title>
 </head>
-
-<?php include("../includes/navigation.php"); ?>
-
 <body>
 
-<?php
-
-$id = $_GET['id'];
-$res = get_group_list($mysqli, '', '', 'id', $id);
-if ($res != 0) {
-    $row = $res->fetch_assoc();
-    if (!$row) {
-        echo "not found";
-    }
-}
-?>
+<?php include("../includes/navigation.php"); ?>
+<?php include_once("../includes/db_connect.php"); ?>
+<?php include_once("../includes/functions.php"); ?>
 
 <div class="content">
-    <center><b>2017 Boston Training on Eldership<br/>
+    <center><b><?php echo $year; ?> Boston Area Training on Eldership<br/>
     Study Group Information</b></center><br/>
 
     <b>Edit Study Group Information</b><br/>
     <div class="application_section">
-        <form method="post" onsubmit="return validateForm()" action="update_member.php"  name="member">
-          <input type="hidden" name="id" value="<? echo "$row[id]"?>" />
+        <form method="post" onsubmit="return validateForm()" action="submit_member.php"  name="member">
             <b>Locality:</b><br/>
             <select id="locality" name="locality">
-                <option value="<? echo "$row[locality]"?>"><? echo "$row[locality]" ?></option>
-<?php echo get_provinces('../includes/localities.txt'); ?>
+                <option value="">----</option>
+                <?php echo get_provinces('../includes/localities.txt'); ?>
 
-            </select><br/><br/>
-        <b>Language:</b><br/>
-        <select id="language" name="language">
-                <option value="<? echo "$row[language]"?>"><?echo "$row[language]"?></option>
-                <option value="English">English</option>
-                <option value="Chinese">Chinese</option>
-                <option value="French">French</option>
             </select><br/><br/>
        <b>Group Number:</b></br>
        <select id="group_no" name="group_no">
-                <option value="<? echo "$row[group_no]"?>"><? echo "$row[group_no]"?></option>
+                <option value=0>----</option>
                 <option value=1>1</option>
                 <option value=2>2</option>
                 <option value=3>3</option>
@@ -91,10 +65,20 @@ if ($res != 0) {
                 <option value=8>8</option>
                 <option value=9>9</option>
                 <option value=10>10</option>
+                <option value=11>11</option>
+                <option value=12>12</option>
+                <option value=13>13</option>
+                <option value=14>14</option>
+                <option value=15>15</option>
+                <option value=16>16</option>
+                <option value=17>17</option>
+                <option value=18>18</option>
+                <option value=19>19</option>
+                <option value=20>20</option>
             </select><br/><br/>
         <b>Group Meeting Day:</b><br/>
         <select id="mtg_day" name="mtg_day">
-        <option value="<? echo "$row[mtg_day]"?>"><? echo "$row[mtg_day]"?></option>
+        <option value=''>-----------</option>
         <option value="Lord&#39s Day">Lord's Day</option>
         <option value="Monday">Monday</option>
         <option value="Tuesday">Tuesday</option>
@@ -104,46 +88,21 @@ if ($res != 0) {
         <option value="Saturday">Saturday</option>
     </select><br/><br/>
         <b>Meeting Time:</b></br>
-        <input type="text" name="mtg_time" id="mtg_time" size="30" value="<? echo "$row[mtg_time]"?>"/><br/></br>
+        <input type="text" name="mtg_time" id="mtg_time" size="30"/><br/></br>
         <b>Meeting Place:</b><br/>
-        <input type="text" name="mtg_place" id="mtg_place" size="30" value="<? echo "$row[mtg_place]"?>"/><br/><br/>
-        <b>Number of Group Members:</b><br/>
-        <select id="member_no" name="member_no">
-        <option value="<? echo "$row[member_no]"?>"><? echo "$row[member_no]"?></option>
-        <option value=1>1</option>
-        <option value=2>2</option>
-        <option value=3>3</option>
-        <option value=4>4</option>
-        <option value=5>5</option>
-        <option value=6>6</option>
-        <option value=7>7</option>
-        <option value=8>8</option>
-        <option value=9>9</option>
-        <option value=10>10</option>
-        <option value=11>11</option>
-        <option value=12>12</option>
-        <option value=13>13</option>
-        <option value=14>14</option>
-        <option value=15>15</option>
-        <option value=16>16</option>
-        <option value=17>17</option>
-        <option value=18>18</option>
-        <option value=19>19</option>
-        <option value=20>20</option>
-        <option value=20>21</option>
-        <option value=20>22</option>
-        <option value=20>23</option>
-        <option value=20>24</option>
-        <option value=20>25</option>
-        <option value=20>26</option>
-        <option value=20>27</option>
-        <option value=20>28</option>
-        <option value=20>29</option>
-        <option value=20>30</option>
-           </select></br></br>
-        <b>Members' Names</b><br/>
-        <textarea name="member_name" id="member_name" rows="4" cols="40"><?echo "$row[member_name]"?></textarea><br/><br/>
-        <input type="submit" value="Edit Study Group Information"/>
+        <input type="text" name="mtg_place" id="mtg_place" size="30"/><br/><br/>
+        <b>Members</b><br/>
+        <?php include_once("../includes/functions.php"); ?>
+        <?php
+            $res = get_all_trainees($mysqli);
+            while ($row = $res->fetch_assoc()) {
+                // print_r ($row);
+                // print $row['first_name'] . $row['last_name'], $row['id'];
+                print '<label> <input type="checkbox" name="member_ids[]" value="' . $row['id'] . '" /> '. $row['first_name'] . " " . $row['last_name'] . "</label><br/>";
+            }
+        ?>
+        <input type="submit" value="Submit Study Group Information"/>
+        <a href="/portal/">Cancel</a>
     </div>
 
 </div>
