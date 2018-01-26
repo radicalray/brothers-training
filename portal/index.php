4fc2fb67d06ac83b5040856b17b9002bba0318b3
Ray <ray_li@mit.edu>
2018-01-25 22:43:18 -0500

    Stripped site to just video page

--- portal/index.php
+++ portal/index.php
@@ -1,104 +1,44 @@
+
+<!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="styles.css" />
-    <title>Boston Area Training on Eldership</title>
+    <meta charset="UTF-8">
+    <title>Media Access</title>
+    <link rel="stylesheet" type="text/css" href="../styles.css" />
+<style>
+body {
+   background-color: #FEFFE6;
+}
+</style>
 </head>
-<body>
 
-<?php include("../includes/navigation.php"); ?>
+<?php
 
-<div class="content">
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+} else {
+    $password = '';
+}
 
-    <div class="application_section">
-      <b>Registration Dates: January 14 to January 22</b>
-    </div>
+if ($password == "1newman")
+{
+    include("./protected_contents.html");
+}
+else
+{
+    if(isset($_POST['password']))
+         echo "<br/><b>Login failed, please try again. Please email admin@churchincambridge.org if you need help.</b><br/><br/>";
+    {?>
+            <form method="POST" action="index.php">
+            Please enter your password to access: <input type="password" name="password"></input><br/>
+            <input type="submit" name="submit" value="Go"></input>
+            <a href="/portal/">Cancel</a>
+            </form>
+    <?php }
+}
 
-    <div class="application_section">
-        <b>Video: Brother Minoru's fellowship in Boston<br/></b>
-        <ul>
-            <li><u><a href="/portal/special_fellowship.php">Special Fellowship to All Registered Trainees</a></u></li>
-            <!--li><u><a href="/brotherstraining/portal/uploads/2016.01.06_Wednesday_Training.mp4">Words to brothers regarding the elders' training</a></u></li>
-            <li><u><a href="/brotherstraining/portal/uploads/2016.01.07_Thursday_Sisters_Meeting.mp4">Special fellowship to the sisters</a></u></li-->
-        </ul>
-    </div>
+?>
 
-
-    <b>Event: Boston Area Training on Eldership</b>
-
-    <div class="application_section">
-    <b><u>Second Term Training Registration</u></b><br/><br/>
-    The registration for second term is <b>OPEN</b>. To register: </br>
-    <ul>
-    <!--li>Existing trainees registering for third term: click <b><a href="/brotherstraining/portal/admin/index.php?username=3rdTerm&task=register"/>HERE</a></b></li-->
-    <li>Step 1: Fill out your application form <b><a href="registration_new.php"/>HERE</a></b></li>
-    <li>Step 2: Fill out your consecration form <b><a href="consecration.php"/>HERE</a></b></li>
-    </ul>
-    <br/>Please email admin@churchincambridge.org if you need help.<br/><br/>
-    </div>
-
-<!--    <div class="application_section">
-    <b><u>Trainee criteria</u></b><br/>
-            <ul>
-		<li>25 years of age or older;
-		<li>Married, with a proper marital status (Single brothers can be considered depending on their level of maturity and faithfulness in service);
-		<li>Of a good character;
-		<li>Matured in life, faithful in service;
-		<li>Have a good knowledge of the truth;
-		<li>Able to coordinate with others;
-		<li>Already serving as an elder or responsible one;
-		<li>Living a normal church life, including regular meeting attendance and functioning on Lord's Day, prayer meeting, group meeting, etc.
-		<li>Willing to commit to the training schedule (i.e. one Saturday a month from 9 am to 1 pm and a regular weekly meeting with your study group members).
-             </ul>
-    </div>
--->
-    <div class="application_section">
-        <b>Video: Burden and Preparation for Training<br/></b>
-        <ul>
-            <li><u><a href="/portal/uploads/2016/MCFellowshipToBrothersInCanada.mp4">Training Burden</a></u></li>
-            <!-- <li><u><a href="./admin/media_access.php">Practical Matters</a></u>*</li>
-            <li><u><a href="./uploads/2014/12/2014DEC14SISMTG.mp3">Sisters Meeting</a></u></li> -->
-        </ul>
-    </div>
-
-
-    <div class="application_section">
-    <b><u>Training Dates <?php global $year; echo $year; ?>:</u></b>
-    <ul>
-    <!-- <li><b>First Term</b> - Jan 28 April 16, May 21, Jun 25, August 6, September 24, October 22</li> -->
-
-    <li><b>Second Term</b> - Jan 28, Mar 11, Apr 15, May 20, Jun 24, Aug 19</li>
-    </ul>
-
-    <b><u>Training Location:</u></b><br/>
-    Newton, MA<br/><br/>
-    <b><u>Time:</u></b><br/>
-    Saturdays 1 PM to 4 PM.<br/><br/>
-
-    <b><u>Cost</u></b></br>
-    $50 USD for the training for 6 months.<br/><br/>
-
-
-    <b><u>Training Guidelines and Regulations</u></b>
-    <ul>
-    <li><b>Attire</b> - A shirt and tie are the recommended attire for each trainee.
-    <li><b>Arrival Time</b> - Trainees are encouraged to arrive early, praying and exercising the spirit:</li>
-       <ul>
-       <li>Every trainee is expected to be in their seat no later than 5 minutes before the start time of each session (12:55 PM).</li>
-       <li>Doors will be locked at the start time of each session. Those who arrive late should expect that they will not be able to enter until the next session break.</li>
-       </ul>
-   <li><b>Training Outlines</b> - Duplication of the training material is not permitted. For any absent trainees, only the local administrator (or an assigned representative) should pick up training outlines at the registration table.</li>
-   <li><b>Attendance</b> - All trainees should arrange their schedule so as not to miss any meeting:</li>
-       <ul>
-       <li>For the monthly training sessions, two excused absences per 6-month period are permitted with prior approval.
-       <li>More than one unexcused absence or more than two excused absences for each 6-month period will result in dismissal from the training.
-       </ul>
-    </ul>
-
-
-    </div>
-
-
-</div>
 
 </body>
 </html>
